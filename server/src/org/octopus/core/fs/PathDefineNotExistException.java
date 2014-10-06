cf679b3829765456287255164c9eef4e10999c82
pangwu86 <pangwu86@gmail.com>
2014-10-06 23:26:28 +0800

    ..

---
+++ server/src/org/octopus/core/fs/PathDefineNotExistException.java
@@ -0,0 +1,10 @@
+package org.octopus.core.fs;
+
+public class PathDefineNotExistException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7400937335070537753L;
+
+}
